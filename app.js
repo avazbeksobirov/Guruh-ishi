71205f81cc1b3de3cd35bb9039b3a343f6dcc2e5
Avazbek <avazsobirov9966@gmail.com>
2024-11-08 22:36:50 +0500

    js daki comentariani o'chirdim

--- app.js
+++ app.js
@@ -1,26 +1,2 @@
 // Guruh bilan
 
-// const cards = Array.from(document.querySelectorAll('.card')); // Barcha kartalarni massivga o‘giramiz
-// const cardContainer = document.getElementById('cardContainer');
-// let startIndex = 0; // Boshlang'ich kartani indexi
-
-// document.getElementById('nextButton').addEventListener('click', () => {
-//     // Kartalarni siljitish uchun birinchi kartani olib tashlaymiz
-//     const firstCard = cards[startIndex];
-    
-//     // O'chish animatsiyasini qo'llaymiz
-//     firstCard.style.opacity = 0;
-    
-//     setTimeout(() => {
-//         // Yangi indeksda keyingi kartani o‘rnatamiz
-//         startIndex = (startIndex + 1) % cards.length;
-        
-//         // Yangi kartani oxiriga qo‘shamiz
-//         const nextCard = cards[startIndex];
-        
-//         // Ko‘rinishini qayta o‘zgartirish
-//         firstCard.style.opacity = 1;
-//         firstCard.remove(); // Birinchi kartani olib tashlaymiz
-//         cardContainer.append(nextCard); // Keyingi kartani oxiriga qo‘shamiz
-//     }, 500); // Animatsiya davomiyligi
-// });
