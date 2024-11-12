f975fede64ea2d67769cdafc024e20a9c294592d
Avazbek <avazsobirov9966@gmail.com>
2024-11-12 16:30:12 +0500

    added js and css

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 // Guruh bilan
 
+document.getElementById('nextButton').addEventListener('click', () => {
+    document.getElementById('cardContainer').scrollBy({
+        left: 300, // Adjust as needed
+        behavior: 'smooth'
+    });
+});
+document.getElementById('backButton').addEventListener('click', () => {
+    document.getElementById('cardContainer').scrollBy({
+        left: -300, // Scrolls 300px to the right
+        behavior: 'smooth'
+    });
+});
